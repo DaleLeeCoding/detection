e65bce499110f5cd98eb1db4577ca4c1575f9c44
SEUNGWOO LEE <69357689+DaleLeeCoding@users.noreply.github.com>
2021-10-04 07:28:17 +0900

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -75,7 +75,7 @@ train_pipeline = [
 ]
 data = dict(train=dict(pipeline=train_pipeline))
 optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(_delete_=True,grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
